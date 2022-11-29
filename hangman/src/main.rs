52c59e31bc0f69aaf7a6cfdac24c7f13a742ac11
ryangordon11235 <78880773+ryangordon11235@users.noreply.github.com>
2022-11-29 16:08:12 +1300

    Program now processes guesses

--- hangman/src/main.rs
+++ hangman/src/main.rs
@@ -1,49 +1,89 @@
+use alphabet_macro::alphabet;
 use rand::seq::SliceRandom;
 use std::io;
 use std::io::Write;
 
 mod words;
 
-const ALPHABET: [&str; 26] = [
-    "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s",
-    "t", "u", "v", "w", "x", "y", "z",
-];
+alphabet!(LAT_ALPHABET = "abcdefghijklmnopqrstuvwxyz");
 
 fn main() {
-    let word: &str = words::get_words().choose(&mut rand::thread_rng()).unwrap();
+    let word: Vec<char> = words::get_words()
+        .choose(&mut rand::thread_rng())
+        .unwrap()
+        .chars()
+        .collect();
 
-    let mut incorrect_letters_guessed: Vec<char> = Vec::new();
-    let mut user_progress: Vec<u8> = vec!['_' as u8; word.len()];
+    let mut incorrect_guesses: Vec<char> = Vec::new();
+    let mut user_progress: Vec<char> = vec!['_'; word.len()];
     let mut guesses_left: i8 = 12;
 
-    println!("{word}");
+    // FOR DEBUGGING PURPOSES ONLY
+    println!("{}", word.iter().collect::<String>());
 
-    // Loop until the user guesses the word.
-    while String::from_utf8_lossy(&user_progress) != word && guesses_left > 0 {
+    // Loop until the user guesses the word or runs out of guesses.
+    while &user_progress.iter().collect::<String>() != &word.iter().collect::<String>()
+        && guesses_left > 0
+    {
         println!(
             "With {} guesses remaining you have successfully guessed {}.\n",
             guesses_left,
-            String::from_utf8_lossy(&user_progress)
+            &user_progress.iter().collect::<String>()
         );
 
         // Print out each incorrect letter guessed, separated by commas.
 
         // Obtain a guess from the user, ensure it is within the alphabet.
-        let mut guess = String::new();
-        while !ALPHABET.iter().any(|x| x == &guess.as_str()) {
-            guess = get_input_with_prompt("Enter your guess: ").to_lowercase();
-
-            if !ALPHABET.iter().any(|x| x == &guess.as_str()) {
-                println!("That's not a letter...");
-            }
+        let mut guess: char = char::default();
+        while !LAT_ALPHABET.contains(&guess) {
+            let input = get_input_with_prompt("Enter your guess: ").to_lowercase();
 
             println!();
+
+            if input.len() == 1 {
+                guess = input.chars().nth(0).unwrap();
+            } else {
+                println!("Please enter a single character.\n");
+
+                continue;
+            }
+
+            if !LAT_ALPHABET.iter().any(|x| x == &guess) {
+                println!("That's not a letter...\n");
+            }
         }
 
-        guesses_left -= 1;
+        // If the user guessed a letter correctly, update their progress
+        if word.contains(&guess) {
+            println!("Correct!\n");
+
+            for letter_index in 0..word.len() {
+                if word[letter_index] == guess {
+                    user_progress[letter_index] = guess;
+                }
+            }
+        } else {
+            println!("Incorrect!\n");
+
+            incorrect_guesses.push(guess);
+
+            guesses_left -= 1;
+        }
     }
 }
 
+/// Uses `io::stdin` to get input from the user preceded by a prompt.
+/// Returns the input provided by the user with as a trimmed `String`.
+///
+/// # Arguments
+///
+/// * `prompt` - A string slice given to the user as an input prompt.
+///
+/// # Examples
+///
+/// ```
+/// let name = get_input_with_prompt("Enter your name: ");
+/// ```
 fn get_input_with_prompt(prompt: &str) -> String {
     let mut buffer = String::new();
 
